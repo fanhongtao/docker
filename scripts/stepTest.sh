d2b4ae6f4fff708031adbd08cffcc9336e8e8d93
Michael Gissing <michael@gissing.at>
2020-09-04 18:19:32 +0200

    more test cases

--- scripts/stepTest.sh
+++ scripts/stepTest.sh
@@ -16,6 +16,15 @@ set -x
 CONTAINER="$(createTestContainerName)"
 runTestContainer "${CONTAINER}" "${TAG}"
 
-curl "http://127.0.0.1:3080/"
+HOST_PORT="127.0.0.1:3080"
+BASE="http://${HOST_PORT}"
+
+curl "${BASE}/" > /dev/null
+curl "${BASE}/Page1.md" > /dev/null
+curl "${BASE}/Page1" > /dev/null
+curl "${BASE}/gollum/search?q=test" > /dev/null
+curl "${BASE}/gollum/search?q=foo" > /dev/null
+curl "${BASE}/Page1_9" > /dev/null
+curl "${BASE}/large.bin" > /dev/null
 
 docker stop "${CONTAINER}"
